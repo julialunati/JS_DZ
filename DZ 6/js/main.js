f1b158f9826247c59affcc445fddfdef556e4331
julialunati <julia.molokova11@icloud.com>
2020-11-01 19:43:52 +0100

    DZ_6

---
+++ DZ 6/js/main.js
@@ -0,0 +1,23 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                        console.log(error);
+                        this.$refs.err.hasError = true;
+                })
+        }
+    },
+
+    mounted() {
+        console.log(this);
+    }
+});
+
